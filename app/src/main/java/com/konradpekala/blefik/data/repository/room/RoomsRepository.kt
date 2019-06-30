41fe88a0aadcb42235f4d8c2e412e9c8f116883f
KonradusPL <konradus2012@gmail.com>
2019-06-30 17:53:22 +0200

    refactor(rooms_repository): clean commented code

--- app/src/main/java/com/konradpekala/blefik/data/repository/room/RoomsRepository.kt
+++ app/src/main/java/com/konradpekala/blefik/data/repository/room/RoomsRepository.kt
@@ -1,6 +1,5 @@
 package com.konradpekala.blefik.data.repository.room
 
-import android.util.Log
 import com.google.firebase.Timestamp
 import com.konradpekala.blefik.data.auth.UserSession
 import com.konradpekala.blefik.data.model.Player
@@ -14,20 +13,6 @@ class RoomsRepository @Inject constructor(private val prefs: Preferences,
                                           private val userSession: UserSession,
                                           private val mRemote: IRoomsRepository.Remote) {
 
-    private var mCurrentRoom: Room? = null
-
-    /*fun addRoom(name: String): Single<String> {
-        val creator = prefs.getUserNick()
-        val creatorId = userSession.getUserId()
-        val imageUrl = prefs.getProfileImageUrl()
-        val player = Player(creatorId,creator,imageUrl)
-        mCurrentRoom = Room(name = name, creatorId = creatorId,createdTime = Timestamp.now())
-        mCurrentRoom?.players?.add(player)
-
-        return database.addRoom(mCurrentRoom!!)
-            .subscribeOn(SchedulerProvider.io())
-            .observeOn(SchedulerProvider.ui())
-    }*/
 
     fun addRoom(room: Room): Completable{
         return mRemote.addRoom(room)
@@ -39,40 +24,10 @@ class RoomsRepository @Inject constructor(private val prefs: Preferences,
     }
 
     fun addPlayerToRoom(room: Room, player: Player): Completable{
-        //mCurrentRoom = room
         return mRemote.addPlayerToRoom(player, room.roomId)
     }
 
     fun changeRoomToStarted(room: Room): Completable{
         return mRemote.changeRoomToStarted(room)
-    }
-
-    fun playerIsInRoom(room: Room): Boolean{
-        val localPlayer = getLocalPlayer()
-        for(player in room.players){
-            if(player.id == localPlayer.id)
-                return true
-        }
-        return false
-    }
-
-    private fun getLocalPlayer(): Player{
-        val creator = prefs.getUserNick()
-        val id = userSession.getUserId()
-        val imageUrl = prefs.getProfileImageUrl()
-        Log.d("local player","$creator $id")
-        return Player(id,creator,imageUrl)
-    }
-
-    fun isSameAsChosenBefore(room: Room): Boolean{
-        if(mCurrentRoom == null)
-            return false
-        else if(mCurrentRoom!!.roomId == room.roomId && mCurrentRoom!!.name == room.name)
-            return true
-        return false
-    }
-
-    fun updateCurrentRoom(room: Room){
-        mCurrentRoom = room
     }
 }
